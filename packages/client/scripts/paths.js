23fea81877b4aaed2affc6d65711183cae28304b
Jamen <jamen1147@gmail.com>
2021-04-23 23:51:14 +1000

    style: remove console log

--- packages/client/scripts/paths.js
+++ packages/client/scripts/paths.js
@@ -4,11 +4,8 @@ const fs = require('fs');
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
-const resolvePackage = (relativePath) => {
-  const p = path.resolve(appDirectory, relativePath);
-  console.log(p);
-  return p;
-};
+const resolvePackage = (relativePath) =>
+  path.resolve(appDirectory, relativePath);
 
 module.exports = {
   componentModule: resolvePackage('../../packages/components'),
